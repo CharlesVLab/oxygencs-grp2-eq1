35e0874fbf73502bc80f2ad591d14cb7dac3b538
samyw10 <97761482+samyw10@users.noreply.github.com>
2024-03-08 15:10:19 -0500

    ajout des test

--- test/test.py
+++ test/test.py
@@ -1,1 +1,8 @@
-## To Implement
+import unittest
+
+class TestAlwaysPass(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
